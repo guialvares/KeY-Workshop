abb28b917ad85a447924e9d97cf019415e714d21
Guilherme <alvares@chalmers.se>
2024-07-02 16:59:17 +0200

    created script.js with index.html content

---
+++ script.js
@@ -0,0 +1,14 @@
+import solidity from "./node_modules/highlightjs-solidity/dist/solidity.es.min.js";
+
+Reveal.initialize({
+    hash: true,
+    defaultTiming: 120,
+    highlight: {
+        beforeHighlight: (hljs) => {
+            hljs.registerLanguage('solidity', solidity);
+        }
+    },
+
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+});
